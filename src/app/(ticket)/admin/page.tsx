9f967ab2c464827f501218d8698fb0cc0c256419
webw1zard <ulugbeknizomov999@gmail.com>
2025-02-13 14:58:45 +0500

    gg9

--- src/app/(ticket)/admin/page.tsx
+++ src/app/(ticket)/admin/page.tsx
@@ -1,201 +1,271 @@
 "use client";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { createClient } from "@/supabase/client";
-import { useEffect, useState } from "react";
 
-interface Ticket {
-  id: number;
-  from: string;
-  to: string;
-  date: string;
-  price: number;
-  count: number;
-}
-
-const BuyTicketPage = () => {
-  const [selectedTicket, setSelectedTicket] = useState<Ticket[]>([]);
-  const [showForm, setShowForm] = useState(false);
-  const [buyerInfo, setBuyerInfo] = useState({
-    name: "",
-    lastname: "",
-    email: "",
-    ticketCount: 1,
-  });
-  const [selectedId, setSelectedId] = useState<number | null>(null);
+const AdminPage = () => {
+  const [tickets, setTickets] = useState<Array<any>>([]);
+  const [from, setFromCity] = useState<string>("");
+  const [to, setToCity] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
+  const [count, setCount] = useState<string>("");
+  const [time, setTime] = useState<string>("");
+  const [date, setDate] = useState<string>("");
+  const [editingTicket, setEditingTicket] = useState<any | null>(null);
   const supabase = createClient();
 
-  useEffect(() => {
-    const ticketData = localStorage.getItem("selectedTicket");
-    if (ticketData) {
-      setSelectedTicket(JSON.parse(ticketData));
-    }
-  }, []);
-
-  const handleBuyClick = (ticketId: number) => {
-    setSelectedId(ticketId);
-    setShowForm(true);
-  };
-
-  const ChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBuyerInfo({ ...buyerInfo, [e.target.name]: e.target.value });
-  };
-
-  const TicketSubmit = async () => {
-    if (!buyerInfo.name || !buyerInfo.lastname || !buyerInfo.email) {
-      alert("Barcha maydonlarni to'ldiring!");
-      return;
-    }
-
-    const ticketToUpdate = selectedTicket.find((t) => t.id === selectedId);
-    if (!ticketToUpdate) {
-      alert("Xatolik: Chipta topilmadi!");
-      return;
-    }
-
-    if (ticketToUpdate.count < buyerInfo.ticketCount) {
-      alert("Yetarlicha chipta yo‘q!");
-      return;
-    }
-
-    const newCount = ticketToUpdate.count - buyerInfo.ticketCount;
-
-    const { error } = await supabase.from("TicketUsers").insert([
-      {
-        Name: buyerInfo.name,
-        LastName: buyerInfo.lastname,
-        Email: buyerInfo.email,
-        Ticket: selectedId,
-        TicketCount: buyerInfo.ticketCount,
-      },
-    ]);
-
+  const fetchTickets = async () => {
+    const { data, error } = await supabase.from("AvtoTicket").select("*");
     if (error) {
-      console.error("Xatolik yuz berdi:", error.message);
-      alert(`Ma'lumotlar qo'shishda xatolik: ${error.message}`);
+      console.error("Error fetching tickets:", error);
+    } else {
+      setTickets(data);
+    }
+  };
+
+  fetchTickets();
+
+  const createTicket = async () => {
+    if (!from || !to || !price || !count || !time || !date) {
+      toast.error("Iltimos, barcha maydonlarni to'ldiring.");
       return;
     }
 
-    alert("Chipta muvaffaqiyatli sotib olindi!");
-
-    const { error: updateError } = await supabase
-      .from("AvtoTicket")
-      .update({ count: newCount })
-      .match({ id: selectedId });
-
-    if (updateError) {
-      console.error(
-        "Chipta miqdorini yangilashda xatolik:",
-        updateError.message
-      );
+    if (from === to) {
+      toast.error("Qayerdan va qayerga shaharlar farqli bo'lishi kerak.");
+      return;
     }
 
-    setShowForm(false);
-    setBuyerInfo({ name: "", lastname: "", email: "", ticketCount: 1 });
+    if (editingTicket) {
+      const { data, error } = await supabase
+        .from("AvtoTicket")
+        .update({ from, to, price, count, time, date })
+        .eq("id", editingTicket.id);
+
+      if (error) {
+        console.error("Update error:", error);
+      } else {
+        setEditingTicket(null);
+        fetchTickets();
+        toast.success("Ticket muvaffaqiyatli tahrirlandi! 🎉");
+      }
+    } else {
+      const { data, error } = await supabase
+        .from("AvtoTicket")
+        .insert([{ from, to, price, count, time, date }]);
+
+      if (error) {
+        console.error("Insert error:", error);
+        toast.error("Ticket qo'shishda xatolik yuz berdi! ❌");
+      } else {
+        toast.success("Ticket muvaffaqiyatli qo'shildi! 🎉");
+        fetchTickets();
+      }
+    }
+
+    setFromCity("");
+    setToCity("");
+    setPrice("");
+    setCount("");
+    setTime("");
+    setDate("");
   };
 
-  if (selectedTicket.length === 0) {
-    return (
-      <p className="text-center mt-32 text-xl text-gray-500">
-        Chipta topilmadi
-      </p>
-    );
+  function editTicket(ticket: any) {
+    setEditingTicket(ticket);
+    setFromCity(ticket.from);
+    setToCity(ticket.to);
+    setPrice(ticket.price);
+    setCount(ticket.count);
+    setTime(ticket.time);
+    setDate(ticket.date);
+  }
+
+  async function deleteTicket(id: number) {
+    const { error } = await supabase.from("AvtoTicket").delete().eq("id", id);
+    if (error) {
+      console.error("Delete error:", error);
+    } else {
+      fetchTickets();
+      toast.success("Ticket muvaffaqiyatli o'chirildi! 🎉");
+    }
   }
 
   return (
     <div>
-      <nav className="bg-white shadow-md fixed w-full top-0 left-0 z-50">
-        <div className="max-w-7xl mx-auto flex items-center justify-between py-4 px-6">
+      <ToastContainer />
+      <nav className="bg-gradient-to-r from-gray-800 via-gray-900 to-black p-3 shadow-lg fixed w-full top-0 left-0 z-50">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
           <div
-            className="text-gray-900 text-3xl font-bold hover:cursor-pointer"
+            className="text-white text-3xl font-bold hover:cursor-pointer"
             onClick={() => (location.href = "/home")}
           >
-            <span className="text-red-600">TICKET</span>BOOKING          </div>
-
-          <button
-            onClick={() => (location.href = "/admin")}
-            className="bg-red-600 text-white px-6 py-2 rounded-lg shadow-md hover:bg-red-700 transition duration-300"
-          >
-            Admin Page
-          </button>
+            <span className="text-red-600">My</span>Logo
+          </div>
+          <div>
+            <button
+              onClick={() => (location.href = "/admin")}
+              className="bg-gradient-to-r from-red-600 to-pink-600 text-white px-6 py-2 rounded-full shadow-lg hover:bg-gradient-to-r hover:from-pink-600 hover:to-red-600 transition duration-300"
+            >
+              Admin Page
+            </button>
+          </div>
         </div>
       </nav>
 
-      <div className="mt-32 w-full max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {selectedTicket.map((ticket) => (
+      <div className="h-screen w-full mt-10 pt-20 p-4 flex items-center justify-center bg-gradient-to-b from-gray-900 to-gray-700">
+        <div className="bg-black w-full max-w-lg p-8 rounded-3xl shadow-2xl">
+          <h1 className="text-4xl font-bold text-center text-white mb-6">
+            Admin Panel
+          </h1>
+          <div className="space-y-6">
+            <select
+              value={from}
+              onChange={(e) => setFromCity(e.target.value)}
+              className="w-full px-4 py-3 border-2 border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+            >
+              <option value="" disabled>
+                Qayerdan
+              </option>
+              {[
+                "Toshkent",
+                "Buxoro",
+                "Farg'ona",
+                "Xorazm",
+                "Surxondaryo",
+                "Jizzax",
+                "Samarqand",
+                "Navoiy",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Qoraqalpog'iston",
+              ].map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+            <select
+              value={to}
+              onChange={(e) => setToCity(e.target.value)}
+              className="w-full px-4 py-3 border-2 border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+            >
+              <option value="" disabled>
+                Qayerga
+              </option>
+              {[
+                "Toshkent",
+                "Buxoro",
+                "Farg'ona",
+                "Xorazm",
+                "Surxondaryo",
+                "Jizzax",
+                "Samarqand",
+                "Navoiy",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Qoraqalpog'iston",
+              ].map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+
+            <input
+              type="text"
+              placeholder="Puli"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className="w-full px-4 py-3 border-2 border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+            />
+
+            <input
+              type="number"
+              min={25}
+              max={51}
+              placeholder="Nechta chipta"
+              value={count}
+              onChange={(e) => setCount(e.target.value)}
+              className="w-full px-4 py-3 border-2 border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+            />
+
+            <select
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+              className="w-full px-4 py-3 border-2 border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+            >
+              <option value="" disabled>
+                Vaqt
+              </option>
+              {["20:00", "21:00", "22:00", "22:30", "23:00", "23:30"].map(
+                (t) => (
+                  <option key={t} value={t}>
+                    {t}
+                  </option>
+                )
+              )}
+            </select>
+
+            <input
+              type="date"
+              min="2025-03-01"
+              max="2025-03-31"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className="w-full px-4 py-3 border-2 border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+            />
+
+            <button
+              onClick={createTicket}
+              className="w-full bg-gradient-to-r from-red-600 to-pink-600 text-white py-3 rounded-lg shadow-lg hover:bg-gradient-to-r hover:from-pink-600 hover:to-red-600 transition"
+            >
+              Ticket Yaratish
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4 w-[1518.9] mx-auto">
+        {tickets.map((ticket: any) => (
           <div
             key={ticket.id}
-            className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition duration-300"
+            className="bg-gradient-to-r from-gray-800 to-gray-900 text-white rounded-2xl shadow-xl p-6 transform transition duration-300 hover:scale-105"
           >
-            <div className="border-b border-gray-300 py-4 flex justify-between">
-              <div>
-                <p className="text-gray-700">
-                  <strong>{ticket.from}</strong> → <strong>{ticket.to}</strong>
-                </p>
-                <p className="text-gray-500">Sana: {ticket.date}</p>
-              </div>
-              <p className="text-red-600 font-bold">{ticket.price} UZS</p>
+            <div className="flex justify-between items-center mb-6">
+              <h2 className="text-2xl font-semibold">
+                {ticket.from} - {ticket.to}
+              </h2>
+              <span className="text-lg">
+                {ticket.date} <br /> {ticket.time}
+              </span>
             </div>
-
-            <div className="mt-6 text-center">
+            <div className="flex justify-between items-center mb-4">
+              <p>Chipta Narxi: {ticket.price} so'm</p>
+              <p>Chipta Soni: {ticket.count}</p>
+            </div>
+            <div className="flex justify-between items-center">
               <button
-                onClick={() => handleBuyClick(ticket.id)}
-                className="bg-red-600 text-white px-6 py-3 rounded-md shadow-md hover:bg-red-700 transition duration-300"
+                onClick={() => editTicket(ticket)}
+                className="bg-gradient-to-r from-yellow-500 to-yellow-600 px-4 py-2 text-white rounded-md shadow-lg hover:bg-gradient-to-r hover:from-yellow-600 hover:to-yellow-500 transition duration-300"
               >
-                Sotib olish
+                Tahrirlash
+              </button>
+              <button
+                onClick={() => deleteTicket(ticket.id)}
+                className="bg-gradient-to-r from-red-500 to-red-600 px-4 py-2 text-white rounded-md shadow-lg hover:bg-gradient-to-r hover:from-red-600 hover:to-red-500 transition duration-300"
+              >
+                O'chirish
               </button>
             </div>
           </div>
         ))}
       </div>
-
-      {showForm && (
-        <div className="mt-6 max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
-          <h2 className="text-center text-xl font-bold mb-4">
-            Ma&apos;lumotlarni kiriting
-          </h2>
-          <input
-            type="text"
-            name="name"
-            placeholder="Ism"
-            value={buyerInfo.name}
-            onChange={ChangeValue}
-            className="w-full mb-2 p-2 border rounded"
-          />
-          <input
-            type="text"
-            name="lastname"
-            placeholder="Familiya"
-            value={buyerInfo.lastname}
-            onChange={ChangeValue}
-            className="w-full mb-2 p-2 border rounded"
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={buyerInfo.email}
-            onChange={ChangeValue}
-            className="w-full mb-2 p-2 border rounded"
-          />
-          <input
-            max={4}
-            min={1}
-            type="number"
-            name="ticketCount"
-            placeholder="Ticket Count"
-            value={buyerInfo.ticketCount}
-            onChange={ChangeValue}
-            className="w-full mb-2 p-2 border rounded"
-          />
-          <button
-            onClick={TicketSubmit}
-            className="w-full bg-red-600 text-white py-2 rounded-md shadow-md hover:bg-red-700 transition duration-300"
-          >
-            Tasdiqlash
-          </button>
-        </div>
-      )}
     </div>
   );
 };
 
-export default BuyTicketPage;
+export default AdminPage;
